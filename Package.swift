657f64bfb0edeaa90dc4010e305599b64cb73520
Vikram <vikram@Kshitijs-MacBook-Pro.local>
2026-02-12 10:56:35 +0530

    Convert POP to Swift Package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "pop",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "pop",
+            targets: ["POP"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "POP",
+            path: "Sources/POP",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        )
+    ]
+)
